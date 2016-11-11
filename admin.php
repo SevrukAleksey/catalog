4c84e15b61341847f1144c14ce325f78764b59af
name <male@.mail.ru>
2016-11-11 20:40:41 +0200

    created a project

---
+++ admin.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	<title>Contact form using Bootstrap 3.3.4</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<meta charset="utf-8">
+	<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+	<link href="css/style.css" rel="stylesheet" type="text/css"/>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	<script src="js/script.js" type="text/javascript"></script>
+    </head>
+    <body>
+	<div class="span7">   
+	    <div class="widget stacked widget-table action-table">
+
+		<div class="widget-header">
+		    <i class="icon-th-list"></i>
+		    <h3>Table</h3>
+		</div> <!-- /widget-header -->
+
+		<div class="widget-content">
+
+		    <table class="table table-striped table-bordered">
+			<thead>
+			    <tr>
+				<th>Name</th>
+				<th>Price</th>
+				<th>About</th>
+				<th>Photo</th>
+				<th>Add</th>
+				<th class="td-actions"></th>
+			    </tr>
+			</thead>
+			<tbody>
+			    <tr>
+				<td>Trident</td>
+				<td>Internet
+				    Explorer 4.0</td>
+				<td>Trident</td>
+				<td class="td-actions">
+				    <a href="javascript:;" class="btn btn-small btn-primary">
+					<i class="btn-icon-only icon-ok"></i>										
+				    </a>
+
+				    <a href="javascript:;" class="btn btn-small">
+					<i class="btn-icon-only icon-remove"></i>										
+				    </a>
+				</td>
+			    </tr>
+			    <tr>
+				<td>Trident</td>
+				<td>Internet
+				    Explorer 5.0</td>
+				<td>Trident</td>
+				<td class="td-actions">
+				    <a href="javascript:;" class="btn btn-small btn-primary">
+					<i class="btn-icon-only icon-ok"></i>										
+				    </a>
+
+				    <a href="javascript:;" class="btn btn-small">
+					<i class="btn-icon-only icon-remove"></i>										
+				    </a>
+				</td>
+			    </tr>
+			    <tr>
+				<td>Trident</td>
+				<td>Internet
+				    Explorer 5.5</td>
+				<td>Trident</td>
+				<td class="td-actions">
+				    <a href="javascript:;" class="btn btn-small btn-primary">
+					<i class="btn-icon-only icon-ok"></i>										
+				    </a>
+
+				    <a href="javascript:;" class="btn btn-small">
+					<i class="btn-icon-only icon-remove"></i>										
+				    </a>
+				</td>
+			    </tr>
+			    <tr>
+				<td>Trident</td>
+				<td>Internet
+				    Explorer 5.5</td>
+				<td>Trident</td>
+				<td class="td-actions">
+				    <a href="javascript:;" class="btn btn-small btn-primary">
+					<i class="btn-icon-only icon-ok"></i>										
+				    </a>
+
+				    <a href="javascript:;" class="btn btn-small">
+					<i class="btn-icon-only icon-remove"></i>										
+				    </a>
+				</td>
+			    </tr>
+			    <tr>
+				<td>Trident</td>
+				<td>Internet
+				    Explorer 5.5</td>
+				<td>Trident</td>
+				<td class="td-actions">
+				    <a href="javascript:;" class="btn btn-small btn-primary">
+					<i class="btn-icon-only icon-ok"></i>										
+				    </a>
+
+				    <a href="javascript:;" class="btn btn-small">
+					<i class="btn-icon-only icon-remove"></i>										
+				    </a>
+				</td>
+			    </tr>
+			    <tr>
+				<td>Trident</td>
+				<td>Internet
+				    Explorer 5.5</td>
+				<td>Trident</td>
+				<td class="td-actions">
+				    <a href="javascript:;" class="btn btn-small btn-primary">
+					<i class="btn-icon-only icon-ok"></i>										
+				    </a>
+
+				    <a href="javascript:;" class="btn btn-small">
+					<i class="btn-icon-only icon-remove"></i>										
+				    </a>
+				</td>
+			    </tr>
+			    <tr>
+				<td>Trident</td>
+				<td>Internet
+				    Explorer 5.5</td>
+				<td>Trident</td>
+				<td class="td-actions">
+				    <a href="javascript:;" class="btn btn-small btn-primary">
+					<i class="btn-icon-only icon-ok"></i>										
+				    </a>
+
+				    <a href="javascript:;" class="btn btn-small">
+					<i class="btn-icon-only icon-remove"></i>										
+				    </a>
+				</td>
+			    </tr>
+			</tbody>
+		    </table>
+
+		</div> <!-- /widget-content -->
+
+	    </div> <!-- /widget -->
+	</div>
+    </body>
+</html>
